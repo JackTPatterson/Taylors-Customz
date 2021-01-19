2d850401d503376255f629d72517ba0c70202609
Patterson <jtyler03@optonline.net>
2021-01-19 09:52:30 -0500

    I was wrong

--- users/static/users/assets/js/index.js
+++ users/static/users/assets/js/index.js
@@ -1,38 +1,35 @@
-
-(function($) {
+(function ($) {
     "use strict";
 
-    $(function() {
+    $(function () {
         var header = $(".start-style");
-        $(window).scroll(function() {
+        $(window).scroll(function () {
             var scroll = $(window).scrollTop();
 
             if (scroll >= 10) {
                 header.removeClass("start-style").addClass("scroll-on");
-                $('#top-navbar').addClass('bg-white');
-                
+
             } else {
                 header.removeClass("scroll-on").addClass("start-style");
-                 $('#top-navbar').removeClass('bg-white');
             }
         });
     });
 
     //Animation
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $("body.hero-anime").removeClass("hero-anime");
 
-        
+       
     });
 
     //Menu On Hover
 
-    $("body").on("mouseenter mouseleave", ".nav-item", function(e) {
+    $("body").on("mouseenter mouseleave", ".nav-item", function (e) {
         if ($(window).width() > 750) {
             var _d = $(e.target).closest(".nav-item");
             _d.addClass("show");
-            setTimeout(function() {
+            setTimeout(function () {
                 _d[_d.is(":hover") ? "addClass" : "removeClass"]("show");
             }, 1);
         }
@@ -40,7 +37,7 @@
 
     //Switch light/dark
 
-    $("#switch").on("click", function() {
+    $("#switch").on("click", function () {
         if ($("body").hasClass("dark")) {
             $("body").removeClass("dark");
             $("#switch").removeClass("switched");
